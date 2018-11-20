f5ea92f75fc3f7da67e9e5774dfb6bc2ed0d3c95
sklee <hadesleeq@gmail.com>
2018-11-20 11:30:07 +0900

    Caching data with Observable.cache

---
+++ src/main/java/chapter8/ResourceManagement.java
@@ -0,0 +1,83 @@
+package chapter8;
+
+import common.Program;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.nio.client.HttpAsyncClient;
+import rx.Observable;
+import rx.apache.http.ObservableHttp;
+import rx.apache.http.ObservableHttpResponse;
+import rx.functions.Action1;
+import rx.functions.Func0;
+import rx.functions.Func1;
+
+import java.io.IOException;
+
+public class ResourceManagement implements Program {
+
+    @Override
+    public String name() {
+        return "Resource management demonstration";
+    }
+
+    @Override
+    public int chapter() {
+        return 8;
+    }
+
+    public Observable<ObservableHttpResponse> request(String url) {
+        Func0<CloseableHttpAsyncClient> resourceFactory = () -> {
+            CloseableHttpAsyncClient client = HttpAsyncClients.createDefault();
+            client.start();
+
+            System.out.println(Thread.currentThread().getName() + " : Created and started the client.");
+            return client;
+        };
+
+        Func1<HttpAsyncClient, Observable<ObservableHttpResponse>> observableFactory = (client) -> {
+            System.out.println(Thread.currentThread().getName() + " : About to create Observable.");
+            return ObservableHttp.createGet(url, client).toObservable();
+        };
+
+        Action1<CloseableHttpAsyncClient> disposeAction = (client) -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + " : Closing the client.");
+                client.close();
+            } catch (IOException e) {
+            }
+        };
+
+        return Observable.using(
+                resourceFactory,
+                observableFactory,
+                disposeAction);
+    }
+
+    @Override
+    public void run() {
+
+        String url = "https://api.github.com/orgs/ReactiveX/repos";
+        Observable<ObservableHttpResponse> response = request(url);
+
+        System.out.println("Not yet subscribed.");
+        Observable<String> stringResponse = response
+                .<String>flatMap(resp -> resp.getContent()
+                        .map(bytes -> new String(bytes, java.nio.charset.StandardCharsets.UTF_8)))
+                .retry(5)
+                .map(String::trim)
+                .cache();
+
+        System.out.println("Subscribe 1:");
+        System.out.println(stringResponse.toBlocking().first());
+
+        System.out.println("Subscribe 2:");
+        System.out.println(stringResponse.toBlocking().first());
+
+    }
+
+    public static void main(String[] args) {
+        new ResourceManagement().run();
+    }
+
+
+}
